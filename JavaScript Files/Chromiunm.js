bc2819f496af12ea2ee7308fe7af9ad2089f228f
Antonio Estrada <Antonio.estrada22@outlook.com>
2026-01-28 17:03:45 -0700

    Add debug console logs to Chromiumn.js

--- JavaScript Files/Chromiunm.js
+++ JavaScript Files/Chromiunm.js
@@ -1,1 +1,12 @@
 console.log("404 not found presents Chromiunm");
+console.log("Hi someone there");
+
+console.log("Estrada Testing");
+
+console.log("New changes made here");
+
+console.log("New changes made by Ben");
+
+console.log("Final change before submission");
+
+// test commit 
